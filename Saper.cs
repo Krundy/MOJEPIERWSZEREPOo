16b21834048a0e4ea01a8684cd6c38e4c659a7e4
pzakaszewski <149328527+pzakaszewski@users.noreply.github.com>
2023-11-25 16:11:16 +0100

    Update Saper.cs
    naprawiony

--- Saper.cs
+++ Saper.cs
@@ -70,7 +70,7 @@ class Program
 
 
 
-                        if (count  0)
+                        if (count == 0)
 
                         {
 
@@ -126,7 +126,7 @@ class Program
 
         char[,] board = new char[height, width];
 
-        Random random = /*??*/
+        Random random = new Random();
 
 
 
@@ -189,8 +189,7 @@ class Program
 
 
         /*??*/
-        return
-        displayBoard();
+        return displayBoard();
 
     }
 
@@ -262,7 +261,7 @@ class Program
 
         {
 
-            for (int j = Math.Max(0, x - 1); j <=(width - 1, x + 1); j++)
+            for (int j = Math.Max(0, x - 1); j <=Math.Min(width - 1, x + 1); j++)
 
             {
 
@@ -364,7 +363,7 @@ class Program
 
 
 
-        return uncoveredCount == /* ??*/ *height - bombCount;
+        return uncoveredCount == width*height - bombCount;
 
     }
 
